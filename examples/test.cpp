ef904ec07800e46bb0befabd0c15b92818f223a7
Armin Veres <armin.veres@hotmail.com>
2024-02-12 17:26:42 +0100

    refactor: simplify registers
    note that i2c is not enabled by default

--- examples/test.cpp
+++ examples/test.cpp
@@ -4,16 +4,19 @@
 
 #include <array>
 #include <cstdio>
-#include <memory>
 
 #include "ImageData.hpp"
 #include "display.hpp"
 #include "fonts.hpp"
+#include "hardware/i2c.h"
 #include "paint.hpp"
 #include "paint_enums.hpp"
 
 [[noreturn]] auto draw_stuff() {
     using namespace pico_oled::paint;
+
+    // NOTE(aver): Watch out, the i2c only works if the resistor is resolderet
+    // pico_oled::Display<pico_oled::eConType::I2C> display;
 
     pico_oled::Display<pico_oled::eConType::SPI> display;
     Paint paint;
@@ -221,18 +224,52 @@ auto init_hw() {
     gpio_put(EPD_DC_PIN, 0);
     gpio_put(EPD_BL_PIN, 1);
 
-#if 1
+#ifdef I2C_SOLDERED
     // I2C Config
-    i2c_init(i2c1, 300 * 1000);
+    i2c_init(I2C_PORT, 100 * 1000);
     gpio_set_function(EPD_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(EPD_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(EPD_SDA_PIN);
     gpio_pull_up(EPD_SCL_PIN);
-#endif  // 0
+#endif
 
     printf("Successfull init\n");
+    return 0;
 }
+
+auto scanI2C() {
+    sleep_ms(250);
+    auto reserved_addr = [](u8 addr) { return (addr & 0x78) == 0 || (addr & 0x78) == 0x78; };
+    printf("\nI2C Bus Scan\n");
+    printf(" 0 1 2 3 4 5 6 7 8 9 A B C D E F\n");
+
+    for (u8 addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr)) {
+            ret = PICO_ERROR_GENERIC;
+        } else {
+            ret = i2c_read_blocking(I2C_PORT, addr, &rxdata, 1, false);
+        }
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : " ");
+    }
+    printf("Done.\n");
+    return 0;
+}
+
 [[noreturn]] auto main() -> int {
     init_hw();
+    // scanI2C();
     draw_stuff();
 }
--- includes/pico-oled/display.hpp
+++ includes/pico-oled/display.hpp
@@ -35,22 +35,28 @@
 #include <hardware/spi.h>
 #include <pico/time.h>
 
+#include <cstdio>
+
 #include "types.hpp"
+
+volatile static const auto SPI_PORT = spi1;
+
+static constexpr auto EPD_DC_PIN = 8;
+static constexpr auto EPD_CS_PIN = 9;
+static constexpr auto EPD_CLK_PIN = 10;
+static constexpr auto EPD_MOSI_PIN = 11;
+static constexpr auto EPD_RST_PIN = 12;
+static constexpr auto EPD_BL_PIN = 13;
 
 static constexpr auto IIC_CMD = 0x00;
 static constexpr auto IIC_RAM = 0x40;
-
-volatile static const auto I2C_PORT = i2c1;
-volatile static const auto SPI_PORT = spi1;
-
-static constexpr auto EPD_RST_PIN = 12;
-static constexpr auto EPD_DC_PIN = 8;
-static constexpr auto EPD_CS_PIN = 9;
-static constexpr auto EPD_BL_PIN = 13;
-static constexpr auto EPD_CLK_PIN = 10;
-static constexpr auto EPD_MOSI_PIN = 11;
-static constexpr auto EPD_SCL_PIN = 7;
-static constexpr auto EPD_SDA_PIN = 6;
+static constexpr auto IIC_ADDR = 0x3D;
+/// I2C port, use default
+volatile static auto I2C_PORT = i2c0;
+/// I2C data
+static constexpr auto EPD_SDA_PIN = 4;
+/// I2C Clock
+static constexpr auto EPD_SCL_PIN = 5;
 
 namespace pico_oled {
 
@@ -61,73 +67,64 @@ constexpr u32 k_imsize = k_width * k_height / 8;
 enum class eConType { I2C, SPI };
 
 template <eConType T>
-class Display {
-   public:
-    enum class eRegisters {
-        DISABLE_DISP_ON = 0xA4,
-        DISABLE_DISP_OFF = 0xA5,
-        TURN_DISP_OFF = 0xAE,
-        TURN_DISP_ON = 0xAF,
-        DISP_COL_NORMAL = 0xa6,
-        DISP_COL_REV = 0xa7,
-        /// 0x00-0x0F
-        SET_LOW_COL_ADR = 0x00,
-        /// 0x10-0x17
-        SET_HIGH_COL_ADR = 0x10,
-        /// Memory addressing mode
-        SET_MEM_HORI_ADDRING = 0x20,
-        SET_MEM_VERT_ADDRING = 0x21,
-        /// Contrast Control MODE, [0x00, 0xFF]
-        CONTRAST_CTRL = 0x81,
-        /// Set segment remap
-        SET_SEGMENT_REMAP_NORM = 0xA0,
-        SET_SEGMENT_REMAP_REV = 0xA1,
-        /// Set Multiplex Ration, followed by value [0x00, 0x7F]
-        SET_MULTIPLEX_RATIO = 0xA8,
-        /// Set Display offset, \[0x00, 0x7F\]
-        SET_DISP_OFFSET = 0xD3,
-        /// DC-DC voltage Converter
-        SET_DCDC_ON = 0xAD,
-        SET_DCDC_PUMP_ON = 0x8A,
-        SET_DCDC_PUMP_OFF = 0x8B,
-        /// Specify page address to load display RAM to page address reg, [0xB0, 0xBF]
-        // SET_PAGE_ADR = 0xB0-0xbf,
-        /// Set Common output scan direction
+struct Display {
+    struct Regs {
+        static constexpr auto DISABLE_DISP_ON = 0xA4;
+        static constexpr auto DISABLE_DISP_OFF = 0xA5;
+        static constexpr auto TURN_DISP_OFF = 0xAE;
+        static constexpr auto TURN_DISP_ON = 0xAF;
+        static constexpr auto DISP_COL_NORMAL = 0xa6;
+        static constexpr auto DISP_COL_REV = 0xa7;
+        // 0x00-0x0F
+        static constexpr auto SET_LOW_COL_ADR = 0x00;
+        // 0x10-0x17
+        static constexpr auto SET_HIGH_COL_ADR = 0x10;
+        // Memory addressing mode
+        static constexpr auto SET_MEM_HORI_ADDRING = 0x20;
+        static constexpr auto SET_MEM_VERT_ADDRING = 0x21;
+        /// Contrast Control MODE; [0x00, 0xFF]
+        static constexpr auto CONTRAST_CTRL = 0x81;
+        // Set segment remap
+        static constexpr auto SET_SEGMENT_REMAP_NORM = 0xA0;
+        static constexpr auto SET_SEGMENT_REMAP_REV = 0xA1;
+        /// Set Multiplex Ration; followed by value [0x00, 0x7F]
+        static constexpr auto SET_MULTIPLEX_RATIO = 0xA8;
+        /// Set Display offset; \[0x00, 0x7F\]
+        static constexpr auto SET_DISP_OFFSET = 0xD3;
+        // DC-DC voltage Converter
+        static constexpr auto SET_DCDC_ON = 0xAD;
+        static constexpr auto SET_DCDC_PUMP_ON = 0x8A;
+        static constexpr auto SET_DCDC_PUMP_OFF = 0x8B;
+        /// Specify page address to load display RAM to page address reg; [0xB0, 0xBF]
+        // SET_PAGE_ADR = 0xB0-0xbf;
+        // Set Common output scan direction
+
         // SET_OSC_DIR = 0xC0 - 0xC8
-        /// Set Display Clock Divide Ratio/Oscillator Frequency: (Double Bytes Command) [0x00, 0xFF]
-        SET_OSC_CLOCK_DIV = 0xD5,
-        /// Set Dis-charge/Pre-charge Period: (Double Bytes Command) [0x00, 0xFF]
-        SET_PRECHARGE_PER = 0xD9,
-        /// Set VCOM Deselect Level: (Double Bytes Command)
-        SET_VCOM_DESEL = 0xDB,
-        /// Set Display Start Line:（Double Bytes Command）
-        SET_START_LINE = 0xDC,
-        /// Read-Modify-Write
-        SET_READ_MOD_WRITE = 0xE0,
-        /// Read-Modify-Write
-        END_READ_MOD_WRITE = 0xEE,
-        NOP = 0xE3,
+        /// Set Display Clock Divide Ratio/Oscillator Frequency: (Double Bytes
+        /// Command) [0x00; 0xFF]
+        static constexpr auto SET_OSC_CLOCK_DIV = 0xD5;
+        /// Set Dis-charge/Pre-charge Period: (Double Bytes Command) [0x00; 0xFF]
+        static constexpr auto SET_PRECHARGE_PER = 0xD9;
+        // Set VCOM Deselect Level: (Double Bytes Command)
+        static constexpr auto SET_VCOM_DESEL = 0xDB;
+        // Set Display Start Line:（Double Bytes Command）
+        static constexpr auto SET_START_LINE = 0xDC;
+        // Read-Modify-Write
+        static constexpr auto SET_READ_MOD_WRITE = 0xE0;
+        // Read-Modify-Write
+        static constexpr auto END_READ_MOD_WRITE = 0xEE;
+        static constexpr auto NOP = 0xE3;
     };
 
-    // auto init() const {
-    //     reset();
-    //     init_regs();
-    //     sleep_ms(200);
-    //     write_to_reg(eRegisters::TURN_DISP_ON);
-    // }
-
-    // auto show(const u8 *imbuf) const {
-    // template <u32 imsize>
-    // auto show(const std::array<u8, imsize> &imbuf) const {
     auto show(const u8 *imbuf) const {
         constexpr auto l_width = k_width / 8;
         // write starting row
-        write_reg_val(0xB0u);
+        write_to_reg(0xB0u);
 
         for (u8 j = 0; j < k_height; ++j) {
             const u8 column = k_height - 1 - j;
-            write_reg_val(get_reg_val(eRegisters::SET_LOW_COL_ADR) + (column & 0x0Fu));
-            write_reg_val(get_reg_val(eRegisters::SET_HIGH_COL_ADR) + (column >> 0x04u));
+            write_to_reg(Regs::SET_LOW_COL_ADR + (column & 0x0Fu));
+            write_to_reg(Regs::SET_HIGH_COL_ADR + (column >> 0x04u));
 
             for (u8 i = 0; i < l_width; ++i) {
                 auto temp = imbuf[i + j * l_width];
@@ -140,11 +137,11 @@ class Display {
     auto clear() const {
         constexpr auto l_width = k_width / 8;
         // write starting row
-        write_reg_val(0xB0u);
+        write_to_reg(0xB0u);
         for (u8 j = 0; j < k_height; ++j) {
             const u8 column = k_height - 1 - j;
-            write_reg_val(get_reg_val(eRegisters::SET_LOW_COL_ADR) + (column & 0x0Fu));
-            write_reg_val(get_reg_val(eRegisters::SET_HIGH_COL_ADR) + (column >> 0x4u));
+            write_to_reg(Regs::SET_LOW_COL_ADR + (column & 0x0Fu));
+            write_to_reg(Regs::SET_HIGH_COL_ADR + (column >> 0x4u));
             for (u16 i = 0; i < l_width; ++i) {
                 write_data(0x00u);
             }
@@ -166,13 +163,12 @@ class Display {
         byte = static_cast<u8>((byte & 0x0f) << 4) | ((byte & 0xf0) >> 4);
         return byte;
     }
-    static auto get_reg_val(const eRegisters reg) -> u8 { return static_cast<u8>(reg); }
 
     Display() {
         reset();
         init_regs();
         sleep_ms(200);
-        write_to_reg(eRegisters::TURN_DISP_ON);
+        write_to_reg(Regs::TURN_DISP_ON);
     };
 
     ~Display() = default;
@@ -182,37 +178,21 @@ class Display {
     Display &operator=(const Display &) = delete;
 
    private:
-    /// Wirte to register, can be subseded by `write_reg_val()` if it takes a (double) byte command
-    auto write_to_reg(const eRegisters reg) const {
-        auto value = get_reg_val(reg);
-
+    /// Wirte to register, can be subseded by a (double) byte command
+    auto write_to_reg(const u8 reg) const {
         if constexpr (T == eConType::SPI) {
             gpio_put(EPD_DC_PIN, 0);
             gpio_put(EPD_CS_PIN, 0);
-            spi_write_blocking(SPI_PORT, &value, 1);
-            // spi_write_blocking(SPI_PORT, &reg, 1);
+            spi_write_blocking(SPI_PORT, &reg, 1);
             gpio_put(EPD_CS_PIN, 1);
 
         } else if constexpr (T == eConType::I2C) {
-            const u8 data[2] = {IIC_CMD, value};
-            i2c_write_blocking(I2C_PORT, 0x3C, data, 2, false);
+            const u8 data[2] = {IIC_CMD, reg};
+            i2c_write_blocking(I2C_PORT, IIC_ADDR, data, 3, false);
         }
     }
 
-    /// Wite Value directly to device, usually in combination with and preceded by `write_to_reg()`
-    auto write_reg_val(const u8 value) const {
-        if constexpr (T == eConType::SPI) {
-            gpio_put(EPD_DC_PIN, 0);
-            gpio_put(EPD_CS_PIN, 0);
-            spi_write_blocking(SPI_PORT, &value, 1);
-            gpio_put(EPD_CS_PIN, 1);
-
-        } else if constexpr (T == eConType::I2C) {
-            const u8 data[2] = {IIC_CMD, value};
-            i2c_write_blocking(I2C_PORT, 0x3C, data, 2, false);
-        }
-    }
-
+    /// Writes data to device, special command for i2c
     auto write_data(const u8 reg) const {
         if constexpr (T == eConType::SPI) {
             gpio_put(EPD_DC_PIN, 1);
@@ -222,47 +202,47 @@ class Display {
 
         } else if constexpr (T == eConType::I2C) {
             const u8 data[2] = {IIC_RAM, reg};
-            i2c_write_blocking(I2C_PORT, 0x3C, data, 2, false);
+            i2c_write_blocking(I2C_PORT, IIC_ADDR, data, 3, false);
         }
     }
 
     auto init_regs() const {
-        write_to_reg(eRegisters::TURN_DISP_OFF);
+        write_to_reg(Regs::TURN_DISP_OFF);
 
-        write_to_reg(eRegisters::SET_LOW_COL_ADR);
-        write_to_reg(eRegisters::SET_HIGH_COL_ADR);
+        write_to_reg(Regs::SET_LOW_COL_ADR);
+        write_to_reg(Regs::SET_HIGH_COL_ADR);
 
-        write_reg_val(0xB0);  // set page address
+        write_to_reg(0xB0);  // set page address
 
-        write_to_reg(eRegisters::SET_START_LINE);
-        write_reg_val(0x00);  // set to beginning
+        write_to_reg(Regs::SET_START_LINE);
+        write_to_reg(0x00);  // set to beginning
 
-        write_to_reg(eRegisters::CONTRAST_CTRL);
-        write_reg_val(0x6f);
-        write_to_reg(eRegisters::SET_MEM_VERT_ADDRING);
+        write_to_reg(Regs::CONTRAST_CTRL);
+        write_to_reg(0x6f);
+        write_to_reg(Regs::SET_MEM_VERT_ADDRING);
 
-        write_to_reg(eRegisters::SET_SEGMENT_REMAP_NORM);
-        write_reg_val(0xC0);  // set com scan direction
-        write_to_reg(eRegisters::DISABLE_DISP_ON);
+        write_to_reg(Regs::SET_SEGMENT_REMAP_NORM);
+        write_to_reg(0xC0);  // set com scan direction
+        write_to_reg(Regs::DISABLE_DISP_ON);
 
-        write_to_reg(eRegisters::DISP_COL_NORMAL);
-        write_to_reg(eRegisters::SET_MULTIPLEX_RATIO);
-        write_reg_val(0x3F);  // 1/64 duty
+        write_to_reg(Regs::DISP_COL_NORMAL);
+        write_to_reg(Regs::SET_MULTIPLEX_RATIO);
+        write_to_reg(0x3F);  // 1/64 duty
 
-        write_to_reg(eRegisters::SET_DISP_OFFSET);
-        write_reg_val(0x60);
+        write_to_reg(Regs::SET_DISP_OFFSET);
+        write_to_reg(0x60);
 
-        write_to_reg(eRegisters::SET_OSC_CLOCK_DIV);
-        write_reg_val(0x41);
+        write_to_reg(Regs::SET_OSC_CLOCK_DIV);
+        write_to_reg(0x41);
 
-        write_to_reg(eRegisters::SET_PRECHARGE_PER);
-        write_reg_val(0x22);
+        write_to_reg(Regs::SET_PRECHARGE_PER);
+        write_to_reg(0x22);
 
-        write_to_reg(eRegisters::SET_VCOM_DESEL);
-        write_reg_val(0x35);
+        write_to_reg(Regs::SET_VCOM_DESEL);
+        write_to_reg(0x35);
 
-        write_to_reg(eRegisters::SET_DCDC_ON);
-        write_to_reg(eRegisters::SET_DCDC_PUMP_ON);
+        write_to_reg(Regs::SET_DCDC_ON);
+        write_to_reg(Regs::SET_DCDC_PUMP_ON);
 
 #if 0
         write_reg(0xAEu); /*turn off OLED display*/
